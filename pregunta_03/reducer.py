80fe390200a522e4a54c433a30871144787055d1
Juan Pablo Aguirre Martínez <jpaguirrem@unal.edu.co>
2023-05-05 19:21:50 -0500

    P1-3

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,22 @@
-#
 # >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys  
+if __name__ == '__main__':
+    lista = []
+    for line in sys.stdin:
+        line = line.strip().split(',')
+        lista.append(line)
+    l = sorted(lista,key=lambda x: x[1])
+    for i in l:
+        sys.stdout.write('{},{}\n'.format(i[0],i[1]))
+
+
+
+
+        
+        
+
+
+
+
+
+        
