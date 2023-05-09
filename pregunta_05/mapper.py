c25b27fe8ae5f98639752abf5e7da3da8460f27d
Juan Pablo Aguirre Martínez <jpaguirrem@unal.edu.co>
2023-05-08 21:50:39 -0500

    T2 vF

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+# Que compute la cantidad de registros por mes para el archivo `data.csv`.
+
+import sys
+if __name__ == '__main__':
+    for line in sys.stdin:
+        line = line.strip().split('   ')
+        a = line[1].split('-')
+        sys.stdout.write('{}\n'.format(a[1]))
--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,23 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    c_value = None
+    maximo = 0
+    minimo = 0
+    for line in sys.stdin:
+        key, val = line.split(",")
+        val = float(val)
+        if key == c_value:
+            if val > maximo:
+                maximo = val
+            if val < minimo:
+                minimo = val
+        else:
+            if c_value is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(c_value, maximo, minimo))
+            c_value = key
+            maximo = val
+            minimo = val
+    sys.stdout.write("{}\t{}\t{}\n".format(c_value, maximo, minimo))
